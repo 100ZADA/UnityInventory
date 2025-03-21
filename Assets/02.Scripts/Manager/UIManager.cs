989ed58cf22001c85b58ce8c47881b47a36ed69b
100ZADA <5528asd@gmail.com>
2025-03-21 15:03:53 +0900

    [Feat] 코드 추가
    # UIManager.cs
     - UI 프로퍼티 추가

--- Assets/02.Scripts/Manager/UIManager.cs
+++ Assets/02.Scripts/Manager/UIManager.cs
@@ -2,20 +2,30 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-    
+
 // 다른 UI를 끄고 키는 용도로 사용
 public class UIManager : MonoBehaviour
 {
-    [Header("UIMainMenu")]
-    [SerializeField] private UIMainMenu uiMainMenu;
-    public GameObject uiMainMenuPanel;
-    
-    [Header("UIMainMenu")]
-    [SerializeField] private UIStat uiStat;
-    public GameObject statPanel;
-    
-    [Header("UIMainMenu")]
-    [SerializeField] private UIInventory uiInventory;
-    public GameObject inventoryPanel;
+    public static UIManager instance;
 
+    [SerializeField] private UIMainMenu uiMainMenu;
+    [SerializeField] private UIStat uiStat;
+    [SerializeField] private UIInventory uiInventory;
+    
+    // 프로퍼티
+    public UIMainMenu UIMainMenuPanel { get => uiMainMenu; set => uiMainMenu = value; }
+    public UIStat UIStat { get => uiStat; set => uiStat = value; }
+    public UIInventory UIInventory { get => uiInventory; set => uiInventory = value; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
