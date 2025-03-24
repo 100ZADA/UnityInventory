721f2673c09e62a49b97a57d7fd58560a3996bb4
100ZADA <5528asd@gmail.com>
2025-03-24 15:01:24 +0900

    [Feat] UISlot 생성
    # UIInventory.cs
     - UISlotPanel 내부에 UISlot Prefab 생성

--- Assets/02.Scripts/UI/UIInventory.cs
+++ Assets/02.Scripts/UI/UIInventory.cs
@@ -7,10 +7,28 @@ using UnityEngine.UI;
 public class UIInventory : MonoBehaviour
 {
     public Button inventoryBackBtn;
-
+    
+    [SerializeField] private UISlot slotPrefab;
+    [SerializeField] private Transform parent;
+    
+    public List<UISlot> slots = new List<UISlot>();
+    
     void Start()
     {
         inventoryBackBtn.onClick.AddListener(CloseInventoryMenu);
+        InitInventory(15);
+    }
+
+    private void InitInventory(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            if (slotPrefab != null)
+            {
+                UISlot uiSlot = Instantiate(slotPrefab, parent);
+                slots.Add(uiSlot);
+            }
+        }
     }
 
     public void CloseInventoryMenu()
--- Assets/02.Scripts/UI/UISlot.cs
+++ Assets/02.Scripts/UI/UISlot.cs
@@ -2,8 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISlot : MonoBehaviour
 {
+    [SerializeField] private Image image;
+    [SerializeField] private Text countText;
     
+    public void SetItem()
+    {
+        
+    }
+
+    public void RefreshUI()
+    {
+        
+    }
 }
